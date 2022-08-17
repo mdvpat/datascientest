044071ddaaac745bd6c25c8f523c94ce29a89ca2
Charmelionag <muradu.iurie.1986@gmail.com>
2022-08-17 07:05:12 +0200

    Create detecter.py

---
+++ docker/detecter.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, jsonify, request
+
+API_HOST = os.environ["API_HOST"]
+API_PORT = os.environ["API_PORT"]
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route("/status")
+def status():
+    pass
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    data = request.json
+    
+    pass
+
+if __name__ == '__main__':
+    app.run(host=API_HOST, port=API_PORT)
