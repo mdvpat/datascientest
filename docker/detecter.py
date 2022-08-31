be8fda67c1dd85cf63f867e9440f12bb9fa6b370
Charmelionag <muradu.iurie.1986@gmail.com>
2022-08-31 09:57:02 +0200

    Update detecter.py

--- docker/detecter.py
+++ docker/detecter.py
@@ -25,50 +25,53 @@ def status():
 
 @app.route("/verify", methods=["POST"])
 def verify():
-    data = request.get_json()
-    print(f"verifying: {data}")
-    ## Model Params
-    model=joblib.load('model.joblib')
-    print(model.estimators_)
-    ## Input Json // csv input will be implemented later
-    df = pd.read_json(input)
-    ### Check structure
+    try:
+        data = request.get_json()
+        print(f"verifying: {data}")
+        ## Model Params
+        model=joblib.load('model.joblib')
+        print(model.estimators_)
+        ## Input Json // csv input will be implemented later
+        df = pd.read_json(input)
+        ### Check structure
 
-    df.head()
-    ## Transform input : to be pipelined
-    df["signup_time"] = pd.to_datetime(df["signup_time"])
-    df["purchase_time"] = pd.to_datetime(df["purchase_time"])
-    df["delta"]=(df["purchase_time"]-df["signup_time"])
+        df.head()
+        ## Transform input : to be pipelined
+        df["signup_time"] = pd.to_datetime(df["signup_time"])
+        df["purchase_time"] = pd.to_datetime(df["purchase_time"])
+        df["delta"]=(df["purchase_time"]-df["signup_time"])
 
-    ## Encodage variables sex,browser et source en numériques
-    df.source = pd.Categorical(df.source)
-    df.sex = pd.Categorical(df.sex)
-    df.browser = pd.Categorical(df.browser)
-    df["source"] = df["source"].cat.codes
-    df["sex"] = df["sex"].cat.codes
-    df["browser"] = df["browser"].cat.codes
+        ## Encodage variables sex,browser et source en numériques
+        df.source = pd.Categorical(df.source)
+        df.sex = pd.Categorical(df.sex)
+        df.browser = pd.Categorical(df.browser)
+        df["source"] = df["source"].cat.codes
+        df["sex"] = df["sex"].cat.codes
+        df["browser"] = df["browser"].cat.codes
 
-    ## Variables à supprimer
-    df=df.drop("user_id",1)
-    df=df.drop("signup_time",1)
-    df=df.drop("purchase_time",1)
-    df=df.drop("device_id",1)
-    df=df.drop("ip_address",1)
-    df=df.drop("is_fraud",1)
-    ### Delta en heures
-    df["delta"]=df["delta"].dt.total_seconds()
-    df["delta"]=pd.to_numeric( df["delta"])/3600
+        ## Variables à supprimer
+        df=df.drop("user_id",1)
+        df=df.drop("signup_time",1)
+        df=df.drop("purchase_time",1)
+        df=df.drop("device_id",1)
+        df=df.drop("ip_address",1)
+        df=df.drop("is_fraud",1)
+        ### Delta en heures
+        df["delta"]=df["delta"].dt.total_seconds()
+        df["delta"]=pd.to_numeric( df["delta"])/3600
 
-    ## Feed input to Model
-    df["is_fraud"]=model.predict(df)
-    #print("\nRandom Forrest report \n",classification_report(df["is_fraud"], model))
+        ## Feed input to Model
+        df["is_fraud"]=model.predict(df)
+        #print("\nRandom Forrest report \n",classification_report(df["is_fraud"], model))
 
 
-    ## Send Answer
-    #ans=df.to_json(orient="index")
-    ans={'is_fraud' : df["is_fraud"]}
-    
-    return jsonify(ans)
+        ## Send Answer
+        #ans=df.to_json(orient="index")
+        ans={'is_fraud' : df["is_fraud"]}
+        
+        return jsonify(ans)
+    except Exception as e:
+        return jsonify({"error": e})
 
 if __name__ == '__main__':
     app.run(host=API_HOST, port=API_PORT)
